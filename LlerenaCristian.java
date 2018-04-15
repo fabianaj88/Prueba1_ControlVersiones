b68318d7dd17ca942441b8d96f9bcde9a4943155
CRISTIANVALENCIARFC <38392038+CRISTIANVALENCIARFC@users.noreply.github.com>
2018-04-15 09:43:36 -0500

    Create LlerenaCristian.java

---
+++ LlerenaCristian.java
@@ -0,0 +1,6 @@
+ public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Hola compañeros...");
+        JOptionPane.showMessageDialog(null, "Como estan");
+    }
+    
