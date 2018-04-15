4e6d3bb5f1993fe6f65451a5b20cfb994e2143ae
CRISTIAN <CRISTIAN@CRISTIAN_RFC>
2018-04-15 10:15:58 -0500

    ya edite algo

--- src/controlversiones_prueba1/ControlVersiones_Prueba1.java
+++ src/controlversiones_prueba1/ControlVersiones_Prueba1.java
@@ -20,7 +20,6 @@ public class ControlVersiones_Prueba1 {
         // TODO code application logic here
         JOptionPane.showMessageDialog(null, "Hola compañeros...");
         JOptionPane.showMessageDialog(null, "Como estan");
-        JOptionPane.showMessageDialog(null, "Soy Cristian");
     }
     
 }
